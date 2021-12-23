9559c0a4f2a630cd57d93dd31e367e5b4d1c8e29
YelyzavetaLukianova <yelyzaveta.lukianova@gmail.com>
2021-12-23 13:11:29 +0300

    message

---
+++ src/redux/contacts/contactsReducers.js
@@ -0,0 +1,60 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { addContact, delContact, changeFilter } from './contactsActions';
+import { combineReducers } from 'redux';
+
+// const itemsReducer = (
+//   state = [
+//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   ],
+//   action,
+// ) => {
+//   switch (action.type) {
+//     case TYPES.ADD:
+//       return [...state, action.payload];
+//     case TYPES.DEL:
+//       return state.filter(items => items.id !== action.payload);
+
+//     default:
+//       return state;
+//   }
+// };
+
+const itemsReducer = createReducer(
+  [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  builder => {
+    builder
+      .addCase(addContact, (state, action) => [...state, action.payload])
+      .addCase(delContact, (state, action) =>
+        state.filter(items => items.id !== action.payload),
+      );
+  },
+);
+
+// const filterReducer = (state = '', action) => {
+//   switch (action.type) {
+//     case TYPES.FILTER:
+//       return action.payload;
+
+//     default:
+//       return state;
+//   }
+// };
+
+const filterReducer = createReducer('', builder => {
+  builder.addCase(changeFilter, (_, action) => action.payload);
+});
+
+const contactsReducer = combineReducers({
+  items: itemsReducer,
+  filter: filterReducer,
+});
+
+export default contactsReducer;
